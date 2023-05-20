0c70394a45a62dadc2d3bbc31a5ab3899f19fb7c
Gabriel <gabrielturing@gmail.com>
2023-05-20 06:39:57 -0300

    feat: ensure that add product to order use case

--- Application/UseCases/AddProductToOrderUseCase.cs
+++ Application/UseCases/AddProductToOrderUseCase.cs
@@ -1,9 +1,40 @@
-﻿using OrderApp.Domain.Interfaces;
+﻿using OrderApp.Application.Interfaces;
+using OrderApp.Domain.Entities;
+using OrderApp.Domain.Interfaces;
 
 namespace OrderApp.Application.UseCases
 {
     public class AddProductToOrderUseCase
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IProductService _productService;
+        
+        public AddProductToOrderUseCase(IOrderRepository orderRepository, IProductService productService)
+        {
+            _orderRepository = orderRepository;
+            _productService = productService;
+        }
+
+        public void Handle(int orderId, int productId, int quantity)
+        {
+            var order = _orderRepository.GetOrderById(orderId);
+            if (order == null)
+            {
+                throw new Exception($"Order with ID {orderId} not found.");
+            }
+            var product = _productService.GetProductById(productId);
+            if (product == null)
+            {
+                throw new Exception($"Product with ID {productId} not found.");
+            }
+            var item = new OrderItem
+            {
+                Product = product,
+                Quantity = quantity,
+                UnitPrice = product.Price,
+            };
+            order.OrderItems.Add(item);
+            _orderRepository.UpdateOrder(order);
+        }
     }
 }
