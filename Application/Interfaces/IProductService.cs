444a369b723704b692f872508247120eb3ee54a6
Gabriel <gabrielturing@gmail.com>
2023-05-20 06:39:27 -0300

    feat: product service interface

---
+++ Application/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using OrderApp.Domain.Entities;
+
+namespace OrderApp.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Product GetProductById(int productId);
+        IEnumerable<Product> GetProducts();
+    }
+}
